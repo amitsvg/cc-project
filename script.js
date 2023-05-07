13366fa646b34eaeff3b28562492ded2d8d196e2
Abhishek Kumar Shaw <avitech2001@gmail.com>
2023-05-07 23:54:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Make an API request to fetch the latest 2 entries from the channel
+fetch('https://api.thingspeak.com/channels/2129271/feeds.json?results=2')
+  .then(response => response.json())
+  .then(data => {
+    // Get the latest entry from the feeds array
+    const latestEntry = data.feeds[0];
+
+    // Update the value of Box 1 with the value of field1 from the latest entry
+    document.getElementById('box1').innerHTML = latestEntry.field1;
+
+    // Update the value of Box 2 with the value of field2 from the latest entry
+    document.getElementById('box2').innerHTML = latestEntry.field2;
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
